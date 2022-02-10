e48c0f2a78d860d1bb5cf862bc87b0f1dc412fd0
Corbin Lounsbury <corbin.lounsbury@gmail.com>
2022-02-09 22:50:08 -0500

    automated_push

--- clone-repo.ps1
+++ clone-repo.ps1
@@ -7,14 +7,18 @@ param (
 $destPath = "c:\temp\clone_repo"
 new-item -path $destPath -itemtype directory
 Set-Location -Path $destPath
+
+# Clones the public directory and cleans it out, sparing the .git folder
 start-process git -ArgumentList "clone https://$($gitUser):$gitToken@github.com/$gitUser/$publicRepo.git" -Wait -NoNewWindow
 Set-Location -Path $destPath\$publicRepo
-start-process git -ArgumentList "pull"-Wait -NoNewWindow
 Get-ChildItem -path $destPath\$publicRepo\* -exclude /git | Remove-Item -Force
-#Remove-Item  -Recurse -Force -Exclude ".git\*"
+
+# Copies the working directory in, commits,  and pushes to the public repo
 Copy-Item -Path $PSScriptRoot\* -Destination $destPath\$publicRepo -Recurse -Force -Exclude .git
 start-process git -ArgumentList "add ." -Wait -NoNewWindow
 start-process git -ArgumentList "commit -m automated_push" -Wait -NoNewWindow
 start-process git -ArgumentList "push -u origin main" -Wait -NoNewWindow
+
+# Goes back to the execution directory and removes the repo clone directory
 Set-Location -Path $PSScriptRoot
 Remove-Item -path $destPath -Recurse -Force
