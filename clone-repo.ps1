7d18e20c90f016538588a814709057439c17ffc2
Corbin Lounsbury <corbin.lounsbury@gmail.com>
2022-02-09 22:46:29 -0500

    automated_push

--- clone-repo.ps1
+++ clone-repo.ps1
@@ -6,11 +6,15 @@ param (
 
 $destPath = "c:\temp\clone_repo"
 new-item -path $destPath -itemtype directory
+Set-Location -Path $destPath
 start-process git -ArgumentList "clone https://$($gitUser):$gitToken@github.com/$gitUser/$publicRepo.git" -Wait -NoNewWindow
-Remove-Item -path $destPath\$publicRepo\* -Recurse -Force
-Copy-Item -Path $PSScriptRoot\* -Destination $destPath\$publicRepo -Recurse -Force
 Set-Location -Path $destPath\$publicRepo
-git add .
-git commit -m "automated push"
-git push -u origin main
+start-process git -ArgumentList "pull"-Wait -NoNewWindow
+Get-ChildItem -path $destPath\$publicRepo\* -exclude /git | Remove-Item -Force
+#Remove-Item  -Recurse -Force -Exclude ".git\*"
+Copy-Item -Path $PSScriptRoot\* -Destination $destPath\$publicRepo -Recurse -Force -Exclude .git
+start-process git -ArgumentList "add ." -Wait -NoNewWindow
+start-process git -ArgumentList "commit -m automated_push" -Wait -NoNewWindow
+start-process git -ArgumentList "push -u origin main" -Wait -NoNewWindow
+Set-Location -Path $PSScriptRoot
 Remove-Item -path $destPath -Recurse -Force
